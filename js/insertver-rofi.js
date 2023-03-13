dc7078af1d6b1bbf56f9ae92a726340247d610aa
rofinafiin <rofinafiisr@gmail.com>
2023-03-13 19:07:55 +0700

    add js

---
+++ js/insertver-rofi.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  Postdata(formData);
+  resetform();
+})
+
+
+function Postdata(formData){
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var object = {};
+formData.forEach(function(value, key){
+    object;
+    object[key] = value;
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: JSON.stringify(object),
+  redirect: 'follow'
+};
+
+fetch("http://bimit-be.ulbi.ac.id/api/v1/create_bimbingan", requestOptions)
+  .then(response => response.json())
+  .then(result => AmbilResponse(result))
+  .catch(error => console.log('error', error));
+}
+
+function AmbilResponse(result) {
+ alert(result)
+}
+
+function resetform(){
+document.getElementById('form').reset();
+}
