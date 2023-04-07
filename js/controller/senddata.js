158e154a68dc8e8e1804557e5bca570ad2b79d0c
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-07 16:37:27 +0700

    setinner

--- js/controller/senddata.js
+++ js/controller/senddata.js
@@ -1,5 +1,5 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import { getValue,hide,show } from "https://jscroot.github.io/element/croot.js";
+import { getValue,hide,show,setInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 export let token = getCookie("login");
@@ -47,7 +47,7 @@ export function pushButton(){
    
     
     if (!isDataValid) {
-      alert('Mohon lengkapi semua field terlebih dahulu!');
+        setInner('Mohon lengkapi semua field terlebih dahulu!');
       return;
     }else{
         
